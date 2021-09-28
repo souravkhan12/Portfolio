98b5ca19ad5c4bafeb00e5cdc4bde401a1e61e1d
souravkhan12 <souravkhan654.com>
2021-09-28 23:49:15 +0530

    portfolio

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Sourav Khan.",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Sourav Khan  and I'm a Student & Enthusiastic Coder!",
+  githubUrl: "https://github.com/souravkhan12",
+  linkedinUrl: "",
+  cvLink: "",
+};
